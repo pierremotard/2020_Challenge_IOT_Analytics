ff493b7db793177b02c0f618e97ab50179ae9288
Zac <zac.sanchez1@gmail.com>
2020-02-29 01:23:13 -0600

    fixed channel data bug and implemented all channel data

--- hd5_utils.py
+++ hd5_utils.py
@@ -18,10 +18,15 @@ def hd5_to_df(filename, directory):
 def get_channel_data(ch_name, filename, directory):
     if filename.endswith(".hdf"):
         f = h5py.File(os.path.join(directory, filename), 'r')
+        if ch_name not in f['DYNAMIC DATA']: return np.array([])
         return np.array(f['DYNAMIC DATA'][ch_name]['MEASURED'])
     else:
         print('Invalid Filename')
         return None
     
-
-        
+def get_all_channel_data(ch_name, directory):
+    channel_data = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".hdf"):
+            channel_data.extend(list(get_channel_data(ch_name, filename, directory)))
+    return np.array(channel_data)
